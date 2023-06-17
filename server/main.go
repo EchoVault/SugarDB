d787489fb5beb751c329ced282392c83ecccfd23
Kelvin Clement Mwinuka <kelvinmwinuka@hotmail.co.uk>
2023-06-17 22:05:29 +0800

    Accept server flags to toggle TLS

--- server/main.go
+++ server/main.go
@@ -1,9 +1,37 @@
 package main
 
 import (
-	"fmt"
+	"errors"
+	"flag"
+	"log"
 )
 
+type Server struct {
+	tls  *bool
+	key  *string
+	cert *string
+}
+
+func (server *Server) Start() error {
+	return errors.New("server start to be implemented")
+}
+
 func main() {
-	fmt.Println("Let's build the server!")
+	tls := flag.Bool("tls", false, "Start the server in TLS mode.")
+	key := flag.String("key", "", "The private key file path.")
+	cert := flag.String("cert", "", "The signed certificate file path.")
+
+	flag.Parse()
+
+	server := &Server{
+		tls:  tls,
+		key:  key,
+		cert: cert,
+	}
+
+	err := server.Start()
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
