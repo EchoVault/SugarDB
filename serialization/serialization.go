3043923c854e8175765383f676887a24d7b8c2b0
Kelvin Clement Mwinuka <kelvinmwinuka@hotmail.co.uk>
2023-07-04 01:40:12 +0800

    Consolidated encode and decode serialization functions into one file

--- serialization/serialization.go
+++ serialization/serialization.go
@@ -1,10 +1,15 @@
 package serialization
 
 import (
+	"bytes"
+
 	"encoding/csv"
 	"errors"
 	"fmt"
 	"strings"
+
+	"github.com/kelvinmwinuka/memstore/utils"
+	"github.com/tidwall/resp"
 )
 
 func tokenize(comm string) ([]string, error) {
@@ -33,4 +38,27 @@ func Encode(comm string) (string, error) {
 	str += "\n"
 
 	return str, nil
+}
+
+func Decode(raw string) ([]string, error) {
+	rd := resp.NewReader(bytes.NewBufferString(raw))
+	res := []string{}
+
+	v, _, err := rd.ReadValue()
+
+	if err != nil {
+		return nil, err
+	}
+
+	if utils.Contains[string]([]string{"SimpleString", "Integer", "Error"}, v.Type().String()) {
+		return []string{v.String()}, nil
+	}
+
+	if v.Type().String() == "Array" {
+		for _, elem := range v.Array() {
+			res = append(res, elem.String())
+		}
+	}
+
+	return res, nil
 }
