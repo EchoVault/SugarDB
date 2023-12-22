515168cdfc8b171d627c5ef9173a31cbc7aeffd9
Kelvin Clement Mwinuka <kelvinmwinuka@hotmail.co.uk>
2023-12-22 23:35:50 +0300

    Implemented set data type

--- src/modules/set/set.go
+++ src/modules/set/set.go
@@ -1,19 +1,39 @@
 package set
 
 type Set struct {
-	members map[interface{}]int
+	members map[interface{}]interface{}
 }
 
 func NewSet(elems []interface{}) *Set {
-	return &Set{}
+	set := &Set{
+		members: make(map[interface{}]interface{}),
+	}
+	for _, e := range elems {
+		set.members[e] = struct{}{}
+	}
+	return set
 }
 
 func (set *Set) Add(elems []interface{}) int {
-	return 0
+	count := 0
+	for _, e := range elems {
+		if set.members[e] == nil {
+			set.members[e] = struct{}{}
+			count += 1
+		}
+	}
+	return count
 }
 
-func (set *Set) Remove(elems interface{}) int {
-	return 0
+func (set *Set) Remove(elems []interface{}) int {
+	count := 0
+	for _, e := range elems {
+		if set.members[e] != nil {
+			delete(set.members, e)
+			count += 1
+		}
+	}
+	return count
 }
 
 func (set *Set) Pop(count int) []interface{} {
@@ -21,21 +41,52 @@ func (set *Set) Pop(count int) []interface{} {
 }
 
 func (set *Set) Contains(v interface{}) bool {
-	return false
+	return set.members[v] != nil
 }
 
-func (set *Set) Union(other []*Set) Set {
-	return Set{}
+func (set *Set) Union(others []*Set) Set {
+	union := *set
+	for _, s := range others {
+		for k, _ := range s.members {
+			union.Add([]interface{}{k})
+		}
+	}
+	return union
 }
 
-func (set *Set) Intersection(other []*Set) Set {
-	return Set{}
+func (set *Set) Intersection(others []*Set) Set {
+	intersection := *set
+	remove := []interface{}{}
+	for _, s := range others {
+		for k, _ := range s.members {
+			if !intersection.Contains(k) {
+				remove = append(remove, k)
+			}
+		}
+	}
+	intersection.Remove(remove)
+	return intersection
 }
 
-func (set *Set) Subtract(other []*Set) Set {
-	return Set{}
+func (set *Set) Subtract(others []*Set) Set {
+	diff := *set
+	remove := []interface{}{}
+	for _, s := range others {
+		for k, _ := range s.members {
+			if diff.Contains(k) {
+				remove = append(remove, k)
+			}
+		}
+	}
+	diff.Remove(remove)
+	return diff
 }
 
-func (set *Set) Move(other *Set) bool {
-	return false
+func (set *Set) Move(destination *Set, v interface{}) int {
+	if set.members[v] == nil {
+		return 0
+	}
+	set.Remove([]interface{}{v})
+	destination.Add([]interface{}{v})
+	return 1
 }
