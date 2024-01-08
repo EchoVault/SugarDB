a8b47d7fdad922081e714271320884d26b0a4974
Kelvin Clement Mwinuka <kelvinmwinuka@hotmail.co.uk>
2024-01-08 22:08:53 +0300

    Created test skeleton for ping commands

---
+++ src/modules/ping/commands_test.go
@@ -0,0 +1,39 @@
+package ping
+
+import (
+	"context"
+	"github.com/kelvinmwinuka/memstore/src/utils"
+	"net"
+	"reflect"
+	"testing"
+)
+
+type args struct {
+	ctx    context.Context
+	cmd    []string
+	server utils.Server
+	conn   *net.Conn
+}
+
+func Test_handlePing(t *testing.T) {
+	tests := []struct {
+		name    string
+		args    args
+		want    []byte
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := handlePing(tt.args.ctx, tt.args.cmd, tt.args.server, tt.args.conn)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("handlePing() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("handlePing() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
