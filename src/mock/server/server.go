e6900e8d7c3e482897fa6a4f7a0a1b5a5a7912c4
Kelvin Clement Mwinuka <kelvinmwinuka@hotmail.co.uk>
2024-01-12 14:18:48 +0800

    Implemented Snapshot and AOF utils.Server interface methods on mock server with No-OP implementations

--- src/mock/server/server.go
+++ src/mock/server/server.go
@@ -9,3 +9,35 @@ type Server struct {
 	keyLocks        map[string]*sync.RWMutex
 	keyCreationLock *sync.Mutex
 }
+
+func (server *Server) TakeSnapshot() error {
+	return nil
+}
+
+func (server *Server) StartSnapshot() {
+	// No-Op
+}
+
+func (server *Server) FinishSnapshot() {
+	// No-Op
+}
+
+func (server *Server) SetLatestSnapshot(msec int64) {
+	// No-Op
+}
+
+func (server *Server) GetLatestSnapshot() int64 {
+	return 0
+}
+
+func (server *Server) StartRewriteAOF() {
+	// No-Op
+}
+
+func (server *Server) FinishRewriteAOF() {
+	// No-Op
+}
+
+func (server *Server) RewriteAOF() error {
+	return nil
+}
