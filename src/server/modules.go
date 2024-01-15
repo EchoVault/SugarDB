2ca24150d912312ace5eff7739dc09475d2a66e1
Kelvin Clement Mwinuka <kelvinmwinuka@hotmail.co.uk>
2024-01-15 12:05:56 +0800

    Remove LoadModules and LoadCommands methods from server/modules.go. The commands will be injected into the server from the caller through the NewServer constructor

--- src/server/modules.go
+++ src/server/modules.go
@@ -4,45 +4,13 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"github.com/echovault/echovault/src/modules/acl"
-	"github.com/echovault/echovault/src/modules/admin"
-	"github.com/echovault/echovault/src/modules/etc"
-	"github.com/echovault/echovault/src/modules/get"
-	"github.com/echovault/echovault/src/modules/hash"
-	"github.com/echovault/echovault/src/modules/list"
-	"github.com/echovault/echovault/src/modules/ping"
-	"github.com/echovault/echovault/src/modules/pubsub"
-	"github.com/echovault/echovault/src/modules/set"
-	"github.com/echovault/echovault/src/modules/sorted_set"
-	str "github.com/echovault/echovault/src/modules/string"
 	"github.com/echovault/echovault/src/utils"
 	"net"
 	"strings"
 )
 
-func (server *Server) LoadCommands(plugin utils.Plugin) {
-	commands := plugin.Commands()
-	for _, command := range commands {
-		server.commands = append(server.commands, command)
-	}
-}
-
-func (server *Server) LoadModules(ctx context.Context) {
-	server.LoadCommands(admin.NewModule())
-	server.LoadCommands(acl.NewModule())
-	server.LoadCommands(pubsub.NewModule())
-	server.LoadCommands(ping.NewModule())
-	server.LoadCommands(get.NewModule())
-	server.LoadCommands(list.NewModule())
-	server.LoadCommands(str.NewModule())
-	server.LoadCommands(etc.NewModule())
-	server.LoadCommands(set.NewModule())
-	server.LoadCommands(sorted_set.NewModule())
-	server.LoadCommands(hash.NewModule())
-}
-
 func (server *Server) GetAllCommands(ctx context.Context) []utils.Command {
-	return server.commands
+	return server.Commands
 }
 
 func (server *Server) GetACL() interface{} {
@@ -54,7 +22,7 @@ func (server *Server) GetPubSub() interface{} {
 }
 
 func (server *Server) getCommand(cmd string) (utils.Command, error) {
-	for _, command := range server.commands {
+	for _, command := range server.Commands {
 		if strings.EqualFold(command.Command, cmd) {
 			return command, nil
 		}
