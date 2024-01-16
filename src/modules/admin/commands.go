1a0b2945da844c731023d8a0a84dfa4d79123dc8
Kelvin Clement Mwinuka <kelvinmwinuka@hotmail.co.uk>
2024-01-16 12:20:02 +0800

    Renamed NewPlugin function to a Command that returns []utils.Command directly

--- src/modules/admin/commands.go
+++ src/modules/admin/commands.go
@@ -26,56 +26,52 @@ func (p Plugin) Description() string {
 	return p.description
 }
 
-func NewModule() Plugin {
-	return Plugin{
-		name:        "AdminCommands",
-		description: "Handle admin/server management commands",
-		commands: []utils.Command{
-			{
-				Command:     "save",
-				Categories:  []string{utils.AdminCategory, utils.SlowCategory, utils.DangerousCategory},
-				Description: "(SAVE) Trigger a snapshot save",
-				Sync:        true,
-				KeyExtractionFunc: func(cmd []string) ([]string, error) {
-					return []string{}, nil
-				},
-				HandlerFunc: func(ctx context.Context, cmd []string, server utils.Server, conn *net.Conn) ([]byte, error) {
-					if err := server.TakeSnapshot(); err != nil {
-						return nil, err
-					}
-					return []byte(utils.OK_RESPONSE), nil
-				},
+func Commands() []utils.Command {
+	return []utils.Command{
+		{
+			Command:     "save",
+			Categories:  []string{utils.AdminCategory, utils.SlowCategory, utils.DangerousCategory},
+			Description: "(SAVE) Trigger a snapshot save",
+			Sync:        true,
+			KeyExtractionFunc: func(cmd []string) ([]string, error) {
+				return []string{}, nil
 			},
-			{
-				Command:     "lastsave",
-				Categories:  []string{utils.AdminCategory, utils.FastCategory, utils.DangerousCategory},
-				Description: "(LASTSAVE) Get unix timestamp for the latest snapshot in milliseconds.",
-				Sync:        false,
-				KeyExtractionFunc: func(cmd []string) ([]string, error) {
-					return []string{}, nil
-				},
-				HandlerFunc: func(ctx context.Context, cmd []string, server utils.Server, conn *net.Conn) ([]byte, error) {
-					msec := server.GetLatestSnapshot()
-					if msec == 0 {
-						return nil, errors.New("no snapshot")
-					}
-					return []byte(fmt.Sprintf(":%d\r\n\r\n", msec)), nil
-				},
+			HandlerFunc: func(ctx context.Context, cmd []string, server utils.Server, conn *net.Conn) ([]byte, error) {
+				if err := server.TakeSnapshot(); err != nil {
+					return nil, err
+				}
+				return []byte(utils.OK_RESPONSE), nil
 			},
-			{
-				Command:     "rewriteaof",
-				Categories:  []string{utils.AdminCategory, utils.SlowCategory, utils.DangerousCategory},
-				Description: "(REWRITEAOF) Trigger re-writing of append process",
-				Sync:        false,
-				KeyExtractionFunc: func(cmd []string) ([]string, error) {
-					return []string{}, nil
-				},
-				HandlerFunc: func(ctx context.Context, cmd []string, server utils.Server, conn *net.Conn) ([]byte, error) {
-					if err := server.RewriteAOF(); err != nil {
-						return nil, err
-					}
-					return []byte(utils.OK_RESPONSE), nil
-				},
+		},
+		{
+			Command:     "lastsave",
+			Categories:  []string{utils.AdminCategory, utils.FastCategory, utils.DangerousCategory},
+			Description: "(LASTSAVE) Get unix timestamp for the latest snapshot in milliseconds.",
+			Sync:        false,
+			KeyExtractionFunc: func(cmd []string) ([]string, error) {
+				return []string{}, nil
+			},
+			HandlerFunc: func(ctx context.Context, cmd []string, server utils.Server, conn *net.Conn) ([]byte, error) {
+				msec := server.GetLatestSnapshot()
+				if msec == 0 {
+					return nil, errors.New("no snapshot")
+				}
+				return []byte(fmt.Sprintf(":%d\r\n\r\n", msec)), nil
+			},
+		},
+		{
+			Command:     "rewriteaof",
+			Categories:  []string{utils.AdminCategory, utils.SlowCategory, utils.DangerousCategory},
+			Description: "(REWRITEAOF) Trigger re-writing of append process",
+			Sync:        false,
+			KeyExtractionFunc: func(cmd []string) ([]string, error) {
+				return []string{}, nil
+			},
+			HandlerFunc: func(ctx context.Context, cmd []string, server utils.Server, conn *net.Conn) ([]byte, error) {
+				if err := server.RewriteAOF(); err != nil {
+					return nil, err
+				}
+				return []byte(utils.OK_RESPONSE), nil
 			},
 		},
 	}
