b1893c11bcfbca5d4f9b766388e60f72b569eebb
Kelvin Clement Mwinuka <kelvinmwinuka@hotmail.co.uk>
2024-02-14 19:10:20 +0800

    Created hash module test file skeleton

---
+++ src/modules/hash/commands_test.go
@@ -0,0 +1,31 @@
+package hash
+
+import (
+	"testing"
+)
+
+func Test_HandleHSET(t *testing.T) {
+	// TODO: test HSET and HSETNX
+}
+
+func Test_HandleHINCRBY(t *testing.T) {
+	// TODO: test HINCRBY and HINCRBYFLOAT
+}
+
+func Test_HandleHGET(t *testing.T) {}
+
+func Test_HandleHSTRLEN(t *testing.T) {}
+
+func Test_HandleHVALS(t *testing.T) {}
+
+func Test_HandleHRANDFIELD(t *testing.T) {}
+
+func Test_HandleHLEN(t *testing.T) {}
+
+func Test_HandleHKeys(t *testing.T) {}
+
+func Test_HandleHGETALL(t *testing.T) {}
+
+func Test_HandleHEXISTS(t *testing.T) {}
+
+func Test_HandleHDEL(t *testing.T) {}
