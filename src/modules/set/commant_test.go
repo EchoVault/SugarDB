2a5e85612b77033053c98b4c3d6c7bf6bbe3ee3a
Kelvin Clement Mwinuka <kelvinmwinuka@hotmail.co.uk>
2024-02-17 01:51:17 +0800

    Created test skeleton for set type

---
+++ src/modules/set/commant_test.go
@@ -0,0 +1,35 @@
+package set
+
+import (
+	"testing"
+)
+
+func Test_HandleSADD(t *testing.T) {}
+
+func Test_HandleSCARD(t *testing.T) {}
+
+func Test_HandleSDIFF(t *testing.T) {}
+
+func Test_HandleSDIFFSTORE(t *testing.T) {}
+
+func Test_HandleSINTER(t *testing.T) {}
+
+func Test_HandleSINTERCARD(t *testing.T) {}
+
+func Test_HandleSINTERSTORE(t *testing.T) {}
+
+func Test_HandleSISMEMBER(t *testing.T) {}
+
+func Test_HandleSMEMBERS(t *testing.T) {}
+
+func Test_HandleSMOVE(t *testing.T) {}
+
+func Test_HandleSPOP(t *testing.T) {}
+
+func Test_HandleSRANDMEMBER(t *testing.T) {}
+
+func Test_HandleSREM(t *testing.T) {}
+
+func Test_HandleSUNION(t *testing.T) {}
+
+func Test_HandleSUNIONSTORE(t *testing.T) {}
