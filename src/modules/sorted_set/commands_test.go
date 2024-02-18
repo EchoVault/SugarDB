a84d1b4d311ba207b164150d665eb38d09008d7b
Kelvin Clement Mwinuka <kelvinmwinuka@hotmail.co.uk>
2024-02-18 22:56:10 +0800

    Created test skeleton for sorted set test suite

--- src/modules/sorted_set/commands_test.go
+++ src/modules/sorted_set/commands_test.go
@@ -1,1 +1,51 @@
 package sorted_set
+
+import (
+	"testing"
+)
+
+func Test_HandleZADD(t *testing.T) {}
+
+func Test_HandleZCARD(t *testing.T) {}
+
+func Test_HandleZCOUNT(t *testing.T) {}
+
+func Test_HandleZLEXCOUNT(t *testing.T) {}
+
+func Test_HandleZDIFF(t *testing.T) {}
+
+func Test_HandleZDIFFSTORE(t *testing.T) {}
+
+func Test_HandleZINCRBY(t *testing.T) {}
+
+func Test_HandleZINTER(t *testing.T) {}
+
+func Test_HandleZINTERSTORE(t *testing.T) {}
+
+func Test_HandleZMPOP(t *testing.T) {}
+
+func Test_HandleZPOP(t *testing.T) {}
+
+func Test_HandleZMSCORE(t *testing.T) {}
+
+func Test_HandleZRANDMEMBER(t *testing.T) {}
+
+func Test_HandleZRANK(t *testing.T) {}
+
+func Test_HandleZREM(t *testing.T) {}
+
+func Test_HandleZSCORE(t *testing.T) {}
+
+func Test_HandleZREMRANGEBYSCORE(t *testing.T) {}
+
+func Test_HandleZREMRANGEBYRANK(t *testing.T) {}
+
+func Test_HandleZREMRANGEBYLEX(t *testing.T) {}
+
+func Test_HandleZRANGE(t *testing.T) {}
+
+func Test_HandleZRANGESTORE(t *testing.T) {}
+
+func Test_HandleZUNION(t *testing.T) {}
+
+func Test_HandleZUNIONSTORE(t *testing.T) {}
