4010306dee97451de71741ff39a6398365e873c9
Kelvin Mwinuka <kelvin_mwinuka@epam.com>
2024-04-01 17:49:20 +0800

    Updated build and test command in go.yml workflow file

--- pkg/echovault/api_admin.go
+++ pkg/echovault/api_admin.go
@@ -1,6 +1,6 @@
 // Copyright 2024 Kelvin Clement Mwinuka
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");s
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
